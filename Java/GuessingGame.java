0688b4a763025e7bdfffe1ee46fbaef285129436
Rahdyce <147444223+Rahdyce@users.noreply.github.com>
2026-02-24 15:33:10 -0500

    Implement guess counter in GuessingGame
    Added a guess counter to track the number of attempts.

--- Java/GuessingGame.java
+++ Java/GuessingGame.java
@@ -6,18 +6,22 @@ public class GuessingGame {
 	Scanner input = new Scanner(System.in);
 	int GuessNumber = (int)(Math.random()*100);
 	int guess = 0;
+	int guessCounter = 0;
 	
 	while(GuessNumber != guess) {
 		System.out.print("Guess a number between 0 and 100: ");
 		guess = input.nextInt();
 	if(GuessNumber == guess) {
 		System.out.println("Wow you actually did it! ");
+		System.out.println("You guessed it in" + guessCounter + " tries");
 		}
 	else if(GuessNumber > guess) {
 		System.out.println("Gotta bring that number up your number was too low ");
-		}
+		guessCounter++;	
+	}
 	else
 		System.out.println("Slow down there that number is too high ");	
+	guessCounter++;
 	}
 }
 	
